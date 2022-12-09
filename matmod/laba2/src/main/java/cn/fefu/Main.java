a312f3914c77ad64906a8501f9af8492826294b1
Aglicheev Alexander <aglicheev.ao@gmail.com>
2022-12-09 11:56:17 +1000

    artyom lox

--- matmod/laba2/src/main/java/cn/fefu/Main.java
+++ matmod/laba2/src/main/java/cn/fefu/Main.java
@@ -11,10 +11,8 @@ public class Main {
     private static final Integer COOLING = 0;
     private static final Integer HEATING = 1;
     private static final Integer T0 = 291;
-    private static final Integer T_max = 483;
-    private static final Integer T_min = 453;
     private static Integer state = HEATING;
-    private static Integer thermostat(double T) {
+    private static Integer thermostat(double T, double T_max, double T_min) {
         if (T >= T_max) {
             state = COOLING;
         } else if (T <= T_min) {
@@ -22,26 +20,36 @@ public class Main {
         }
         return state.equals(COOLING) ? 0 : 1;
     }
-    private static class Func implements Function<Double, Double> {
-        @Override
-        public Double apply(Double T) {
-            double sigma = 5.67E-8;
-            int P = 1600;
-            int c = 900;
-            double m = 0.8;
-            double s = 0.01;
-            int k = 25;
-            return (P * thermostat(T)- k * s *(T - T0) - s * sigma *(Math.pow(T, 4) - Math.pow(T0, 4))) / (c * m);
-        }
+    private static Double diffEq(Double T, int P, double c, double m, double S) {
+        double sigma = 5.67E-8;
+        int k = 25;
+        return (P - k * S *(T - T0) - S * sigma *(Math.pow(T, 4) - Math.pow(T0, 4))) / (c * m);
     }
 
-    private static ArrayList<Double> euler(Function<Double, Double> func, ArrayList<Integer> t) {
+    private static Double diffEq(Double T, double T_max, double T_min, int P, double c, double m, double S) {
+        double sigma = 5.67E-8;
+        int k = 25;
+        return (P * thermostat(T, T_max, T_min)- k * S *(T - T0) - S * sigma *(Math.pow(T, 4) - Math.pow(T0, 4))) / (c * m);
+    }
+
+    private static ArrayList<Double> euler(ArrayList<Integer> t, int P, double c, double m, double S) {
         ArrayList<Double> res = new ArrayList<>();
 
         double T = T0.doubleValue();
         for (int tValue: t) {
-            T += func.apply(T);
-            res.add(T);
+            T += diffEq(T, P, c, m, S);
+            res.add(T-273);
+        }
+        return res;
+    }
+
+    private static ArrayList<Double> euler(ArrayList<Integer> t, double T_max, double T_min, int P, double c, double m, double S) {
+        ArrayList<Double> res = new ArrayList<>();
+
+        double T = T0.doubleValue();
+        for (int tValue: t) {
+            T += diffEq(T, T_max, T_min, P, c, m, S);
+            res.add(T-273);
         }
         return res;
     }
@@ -52,13 +60,28 @@ public class Main {
             t.add(i);
         }
 
-        ArrayList<Double> T = euler(new Func(), t);
+//        ArrayList<Double> T1 = euler(t, 493, 463,500, 530, 0.8, 0.02);
+//        ArrayList<Double> T2 = euler(t, 443, 423,500, 530, 0.8, 0.02);
+
+//        ArrayList<Double> T1 = euler(t, 493,463,500, 900, 0.8, 0.02);
+//        ArrayList<Double> T2 = euler(t, 443, 423,500, 900, 0.8, 0.02);
+
+//        ArrayList<Double> T1 = euler(t, 493, 463,500, 530, 0.8, 0.01);
+//        ArrayList<Double> T2 = euler(t, 443, 423,500, 530, 0.8, 0.01);
+
+//        ArrayList<Double> T1 = euler(t, 493, 463,500, 530, 1.0, 0.02);
+//        ArrayList<Double> T2 = euler(t, 443, 423,500, 530, 1.0, 0.02);
+
+        ArrayList<Double> T1 = euler(t,493,463, 700, 530, 0.8, 0.02);
+        ArrayList<Double> T2 = euler(t,443,42   3, 700, 530, 0.8, 0.02);
 
         Plot plt = Plot.create();
-        plt.plot().add(t, T);
+        plt.plot().add(t, T1).label("T_max = 220");
+        plt.plot().add(t, T2).label("T_max = 170");
         plt.xlabel("t");
         plt.ylabel("T");
-        plt.savefig("fig8.svg");
+        plt.legend();
+        plt.savefig("fig6.svg");
         plt.show();
     }
 }
